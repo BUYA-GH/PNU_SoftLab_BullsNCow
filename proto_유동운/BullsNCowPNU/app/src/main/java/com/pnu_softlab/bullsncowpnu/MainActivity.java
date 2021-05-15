47975cb4752b41cd4e1b2bcfd59fee679d523076
ydw98 <64944545+ydw98@users.noreply.github.com>
2021-05-15 21:41:37 +0900

    일단 한거올림
    ㅇㅇ

---
+++ proto_유동운/BullsNCowPNU/app/src/main/java/com/pnu_softlab/bullsncowpnu/MainActivity.java
@@ -0,0 +1,102 @@
+package com.pnu_softlab.bullsncowpnu;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import com.google.android.gms.maps.internal.IMapFragmentDelegate;
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.CameraPosition;
+import com.naver.maps.map.LocationTrackingMode;
+import com.naver.maps.map.MapFragment;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.NaverMapOptions;
+import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.UiSettings;
+import com.naver.maps.map.overlay.LocationOverlay;
+import com.naver.maps.map.overlay.OverlayImage;
+import com.naver.maps.map.util.FusedLocationSource;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity
+        implements OnMapReadyCallback {
+
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1000;
+    private FusedLocationSource locationSource;
+    private NaverMap naverMap;
+
+    public TextView eText;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        //위에서 받아서 스트링만들고 그거 출력
+        eText = (TextView)findViewById(R.id.textView);
+        //eText.setText();
+
+        locationSource =
+                new FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE);
+        CameraPosition cameraPosition = new CameraPosition(
+                new LatLng(35.1798159, 129.0750222),
+                0,
+                20,
+                0
+        );
+
+        NaverMapOptions options = new NaverMapOptions()
+                .camera(cameraPosition)
+                .locationButtonEnabled(true);
+
+
+        FragmentManager fm = getSupportFragmentManager();
+        MapFragment mapFragment = (MapFragment)fm.findFragmentById(R.id.map);
+
+        if (mapFragment == null) {
+            mapFragment = MapFragment.newInstance(options);
+            fm.beginTransaction().add(R.id.map, mapFragment).commit();
+        }
+
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,  @NonNull int[] grantResults) {
+        if (locationSource.onRequestPermissionsResult(
+                requestCode, permissions, grantResults)) {
+            return;
+        }
+        super.onRequestPermissionsResult(
+                requestCode, permissions, grantResults);
+    }
+
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+        this.naverMap = naverMap;
+        naverMap.setLocationSource(locationSource);
+        naverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
+
+        List<Integer> pinlist = new ArrayList<Integer>();
+        //gps로 위치추적하는거 만들어야되
+        //이거 핀찾는데에 쓸수있을거 같음
+        naverMap.addOnLocationChangeListener(location ->
+        {
+            naverMap.setLocationSource(locationSource);
+            naverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
+            for(int pin : pinlist)
+                ;
+        });
+
+    }
+}
