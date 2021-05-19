05aca59b22536dbe112251a36421b749757d478c
ydw98 <64944545+ydw98@users.noreply.github.com>
2021-05-19 18:45:46 +0900

    tmp
    tmp

--- UDW/BullsNCowPNU/app/src/main/java/com/pnu_softlab/bullsncowpnu/MainActivity.java
+++ UDW/BullsNCowPNU/app/src/main/java/com/pnu_softlab/bullsncowpnu/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -34,10 +35,9 @@ public class MainActivity extends AppCompatActivity
 
     public TextView eText;
 
-    //pin 관련
-    HashMap<String,pin> Pins = new HashMap<>();
-    HashMap<String,Marker> Markers = new HashMap<>();
-    HashMap<Integer, String> map = new HashMap<>();
+    //pin 관련 나중에 pin지우고 LatLng로 교체하고 밑에 고치는게 나을듯
+    public HashMap<String,pin> Pins = new HashMap<>();
+    public HashMap<String,Marker> Markers = new HashMap<>();
 
     class pin{
         double latitude;
@@ -70,7 +70,7 @@ public class MainActivity extends AppCompatActivity
                 new FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE);
         CameraPosition cameraPosition = new CameraPosition(
                 new LatLng(35.230974, 129.082301),
-                0,
+                16,
                 0,
                 0
         );
@@ -155,8 +155,10 @@ public class MainActivity extends AppCompatActivity
 
         naverMap.setMinZoom(14.0);
         naverMap.setMaxZoom(18.0);
-        naverMap.setExtent(new LatLngBounds(new LatLng(35.230684, 129.075435), new LatLng(35.237382,129.084256)));
+        naverMap.setExtent(new LatLngBounds(new LatLng(35.230009, 129.074514), new LatLng(35.237382,129.084256)));
 
+
+        //나중에 서버에서 받은것들만 처리
         Iterator<Map.Entry<String, pin>> entries = Pins.entrySet().iterator();
         while(entries.hasNext()) {
             Map.Entry<String, pin> entry = entries.next();
@@ -165,12 +167,10 @@ public class MainActivity extends AppCompatActivity
             marker.setWidth(70);
             marker.setHeight(70);
             marker.setIconTintColor(Color.RED);
+
             marker.setMap(naverMap);
             Markers.put(entry.getKey(), marker);
         }
-        //Marker marker = new Marker();
-        //marker.setPosition(new LatLng(37.5670135, 126.9783740));
-        //marker.setMap(naverMap);
 
 
         //gps로 위치추적하는거 만들어야되
@@ -178,22 +178,43 @@ public class MainActivity extends AppCompatActivity
         naverMap.addOnLocationChangeListener(location ->
         {
             naverMap.setLocationSource(locationSource);
+
+            String tmp = new String();
             Utmk locationUtmk = Utmk.valueOf(new LatLng(location.getLatitude(),location.getLongitude()));
             Iterator<Map.Entry<String, pin>> entriesA = Pins.entrySet().iterator();
             while(entriesA.hasNext()){
                 Map.Entry<String, pin> entry = entriesA.next();
                 Utmk entryUtmk = Utmk.valueOf(new LatLng(entry.getValue().latitude, entry.getValue().longitude));
-                if(Math.pow(entryUtmk.x - locationUtmk.x, 2) + Math.pow(entryUtmk.y - locationUtmk.y, 2) < 100) {
+                if((Math.pow(entryUtmk.x - locationUtmk.x, 2) + Math.pow(entryUtmk.y - locationUtmk.y, 2)) <= Math.pow(15,2)) {
                     //공격권주고 서버로 처리
-                    //갔다와서 이거 치우기
-                    //Pins.remove(entry.getKey());
-                    //Markers.get(entry.getKey()).setMap(null);
-                    //Markers.remove(entry.getKey());
+
+
                     //
+                    Intent intent = new Intent(this, PopupActivity.class);
+                    intent.putExtra("data", "Test Popup");
+                    startActivityForResult(intent, 1);
+                    Markers.get(entry.getKey()).setMap(null);
+                    Markers.remove(entry.getKey());
+                    tmp = entry.getKey();
                     Toast.makeText(MainActivity.this, "Correct Answer, Please wait", Toast.LENGTH_SHORT).show();
+                    break;
                 }
+
             }
+            Pins.remove(tmp);
         });
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK) {
+                //데이터 받기
+                String result = data.getStringExtra("result");
+                eText.setText(result);
+            }
+        }
     }
 }
